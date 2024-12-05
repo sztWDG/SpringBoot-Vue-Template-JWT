dd52dcf8a3021a081ed717f5618bb999b1011e19
lionhead <37440154@gitlab.com>
2024-12-05 16:27:49 +0800

    后端：头像上传设置(完善1)

---
+++ my-project-backend/src/main/java/com/example/controller/ObjectController.java
@@ -0,0 +1,59 @@
+package com.example.controller;
+
+import com.example.entity.RestBean;
+import com.example.service.ImageService;
+import io.minio.errors.ErrorResponseException;
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.plaf.TreeUI;
+import java.io.IOException;
+
+@Slf4j
+@RestController
+public class ObjectController {
+
+    @Resource
+    ImageService imageService;
+
+    @GetMapping("/images/avatar/**")
+    //优化：相应图片，设置特殊响应头，为一些比较固定的参数设置一个缓存周期，避免频繁访问后端，优化程序
+    public void imageFetch(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        this.fetchImage(request, response);
+
+    }
+
+    //优化：相应图片，设置特殊响应头，为一些比较固定的参数设置一个缓存周期，避免频繁访问后端，优化程序
+    private void fetchImage(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        //获取用户要请求的图片，去掉前七个：/images，剩下的就是minio里面存储的路径
+        String imagePath = request.getServletPath().substring(7);
+        ServletOutputStream stream = response.getOutputStream();
+
+        if (imagePath.length() <= 13){
+            //UUID自动生成绝对大于13，小于的直接扔掉
+            stream.println(RestBean.failure(404, "Not Found").toString());
+        }else {
+            try {
+                imageService.fetchImageFromMinio(stream, imagePath);
+                response.setHeader("Cache-Control", "max-age=2592000");
+
+            }catch (ErrorResponseException e){//Minio的错误检测
+                if (e.response().code() == 404){
+                    response.setStatus(404);
+                    stream.println(RestBean.failure(404, "Not Found").toString());
+                } else {
+                    log.error("从Minio获取图片出现异常："+e.getMessage(), e);
+                }
+            }
+        }
+    }
+}
--- my-project-backend/src/main/java/com/example/service/impl/ImageImpl.java
+++ my-project-backend/src/main/java/com/example/service/impl/ImageImpl.java
@@ -4,14 +4,21 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.example.entity.dto.Account;
 import com.example.mapper.AccountMapper;
 import com.example.service.ImageService;
+import io.minio.GetObjectArgs;
+import io.minio.GetObjectResponse;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.errors.*;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 @Slf4j
@@ -23,6 +30,16 @@ public class ImageImpl implements ImageService {
 
     @Resource
     AccountMapper accountMapper;
+
+    @Override
+    public void fetchImageFromMinio(OutputStream stream, String image) throws Exception {
+        GetObjectArgs args = GetObjectArgs.builder()
+                .bucket("forum")
+                .object(image)
+                .build();
+        GetObjectResponse response = minioClient.getObject(args);
+        IOUtils.copy(response,stream);
+    }
 
     @Override
     public String uploadAvatar(MultipartFile file, int id) throws IOException{
