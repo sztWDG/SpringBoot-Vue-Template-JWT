42bc1853915facd2d07482fdced5214182153d35
lionhead <657116847@qq.com>
2025-05-03 17:43:24 +0800

    feat:管路端路由守卫
    只有admin角色可以访问admin路径的页面

--- my-project-frontend/src/router/index.js
+++ my-project-frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import {isUnauthorized} from "@/net";
+import {isRoleAdmin, isUnauthorized} from "@/net";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -61,10 +61,13 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-    const unauthorized = isUnauthorized()
+    const unauthorized = isUnauthorized(), admin = isRoleAdmin();
     if (to.name.startsWith('welcome') && !unauthorized) {
         next('/index')
-    } else if (to.fullPath.startsWith('/index') && unauthorized) {
+    } else if (to.fullPath.startsWith('/admin') && !admin) {
+        next('/index')
+    }
+    else if (to.fullPath.startsWith('/index') && unauthorized) {
         next('/')
     } else {
         next()
